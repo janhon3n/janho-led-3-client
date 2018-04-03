123610635ead2a731499a9c3c1ee3da40a9096d6
Mikael Janhonen <janhon3n@gmail.com>
2018-04-04 00:21:16 +0300

    Add Program.js

---
+++ src/Program.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Paper from 'material-ui/Paper'
+
+export default class Program extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return <Paper />
+  }
+}
