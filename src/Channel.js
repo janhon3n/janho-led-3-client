2abf865427b566b3dcc40f3977cf1a3fafa62c0f
Mikael Janhonen <janhon3n@gmail.com>
2018-04-04 17:10:48 +0300

    Update Manual.js

--- src/Channel.js
+++ src/Channel.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import * as Color from 'color'
 import Paper from 'material-ui/Paper'
-import Grid from 'material-ui/Grid'
 import Tabs, { Tab } from 'material-ui/Tabs'
 import Manual from './Manual'
 import Program from './Program'
 import { withStyles } from 'material-ui/styles'
+import io from 'socket.io-client'
 
 const styles = theme => ({
+  Channel: {
+    overflow: 'hidden',
+    margin: '20px',
+  },
   Tabs: {
-    backgroundColor: theme.palette.primary.light,
+    backgroundColor: theme.palette.primary.main,
   },
 })
 
@@ -17,7 +22,23 @@ class Channel extends Component {
     super(props)
 
     this.state = {
-      mode: 0,
+      mode: 1,
+      activeProgram: {
+        type: 'hold',
+        color: { r: 255, g: 0, b: 0 },
+      },
+      programList: [
+        {
+          type: 'hold',
+          color: { r: 255, g: 0, b: 0 },
+          duration: 2,
+        },
+        {
+          type: 'hold',
+          color: { r: 255, g: 0, b: 0 },
+          duration: 1,
+        },
+      ],
     }
   }
 
@@ -25,9 +46,26 @@ class Channel extends Component {
     this.setState({ mode: value })
   }
 
+  holdColor = color => {
+    this.setState({
+      activeProgram: {
+        type: 'hold',
+        color: color,
+      },
+    })
+  }
+
+  editProgramList = newList => {
+    this.setState({ programList: newList })
+  }
+
   render() {
+    let colorRgb = Color(this.state.activeProgram.color).hex()
     return (
-      <Paper>
+      <Paper
+        className={this.props.classes.Channel}
+        style={{ borderLeft: '10px solid ' + colorRgb }}
+      >
         <Tabs
           className={this.props.classes.Tabs}
           value={this.state.mode}
@@ -35,9 +73,20 @@ class Channel extends Component {
         >
           <Tab label="Manual color" />
           <Tab label="Program" />
+          <Tab label="Off" />
         </Tabs>
-        {this.state.mode === 0 && <Manual />}
-        {this.state.mode === 0 && <Program />}
+        {this.state.mode === 0 && (
+          <Manual
+            color={this.state.activeProgram.color}
+            setColor={this.holdColor}
+          />
+        )}
+        {this.state.mode === 1 && (
+          <Program
+            programList={this.state.programList}
+            onEdit={this.editProgramList}
+          />
+        )}
       </Paper>
     )
   }
--- src/Manual.js
+++ src/Manual.js
@@ -3,25 +3,32 @@ import Paper from 'material-ui/Paper'
 import { withStyles } from 'material-ui/styles'
 
 import ColorPicker from './ColorPicker'
+import ColorPreview from './ColorPreview'
 
 const styles = theme => ({
   Manual: {
     padding: '15px',
+    display: 'flex',
+  },
+  Preview: {
+    flexGrow: 1,
+    maxWidth: '100px',
   },
 })
 
 class Manual extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {}
+  setColor = color => {
+    this.props.setColor(color)
   }
 
   render() {
     return (
       <Paper className={this.props.classes.Manual}>
-        Manual mode
-        <ColorPicker />
+        <ColorPreview
+          className={this.props.classes.Preview}
+          color={this.props.color}
+        />
+        <ColorPicker color={this.props.color} onChange={this.setColor} />
       </Paper>
     )
   }
